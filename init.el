2281708c7c4e4770964119d1bc524d17f4de36c7
Nick <nick@monksoftware.co.uk>
2016-04-02 17:15:53 +0100

    Additional config for clojure mode

--- init.el
+++ init.el
@@ -5,8 +5,10 @@
 (defvar my-packages '(better-defaults
 		      projectile
 		      paredit
+                      rainbow-delimiters
                       clojure-mode
-		      cider))
+		      cider
+                      magit))
 
 (dolist (p my-packages)
   (unless (package-installed-p p)
@@ -14,3 +16,11 @@
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/lib/noctilux-theme")
 (load-theme 'noctilux t)
+
+(add-hook 'clojure-mode-hook 'enable-paredit-mode) ;enable paredit for clojure
+(add-hook 'clojure-mode-hook 'subword-mode)        ;camel case working
+;; Use clojure mode for other extensions
+(add-to-list 'auto-mode-alist '("\\.edn$" . clojure-mode))
+(add-to-list 'auto-mode-alist '("\\.boot$" . clojure-mode))
+(add-to-list 'auto-mode-alist '("\\.cljs.*$" . clojure-mode))
+(add-to-list 'auto-mode-alist '("lein-env" . enh-ruby-mode))
